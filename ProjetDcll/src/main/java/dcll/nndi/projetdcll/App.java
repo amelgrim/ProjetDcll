04dde36e6e5591dc9b70e260e960aae3a1d2c4c0
marianendiaye <ndmariane@gmail.com>
2013-02-24 20:22:38 +0100

    Creation du projet

---
+++ ProjetDcll/src/main/java/dcll/nndi/projetdcll/App.java
@@ -0,0 +1,13 @@
+package dcll.nndi.projetdcll;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
