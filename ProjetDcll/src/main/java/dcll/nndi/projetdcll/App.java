a639b679db0c77761ccd7cab074a1f99414ff42a
Diallo Abdourahamane <abdd.diallo@gmail.com>
2013-03-06 12:33:42 +0100

    my eclipse commit

--- ProjetDcll/src/main/java/dcll/nndi/projetdcll/App.java
+++ ProjetDcll/src/main/java/dcll/nndi/projetdcll/App.java
@@ -13,5 +13,7 @@ public class App
         System.out.println( "Hello World! Diallo" );
         System.out.println( "Hello World! Diallo" );
         System.out.println( "Hello World! Diallo" );
+        System.out.println( "Hello World! Diallo5" );
+
     }
 }
