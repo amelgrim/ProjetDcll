627ba94780657667245aa1c5b070bde5d4354170
etu5 <ndmariane@gmail.com>
2013-03-08 09:57:30 +0100

    mise a jour classe App

--- ProjetDcll/src/main/java/dcll/nndi/projetdcll/App.java
+++ ProjetDcll/src/main/java/dcll/nndi/projetdcll/App.java
@@ -6,18 +6,16 @@ package dcll.nndi.projetdcll;
  * @author Ndèye Mariane NDIAYE & Mohamed Amine LOUHIDI
  */
 public class App {
-	/**
-	 * 1.On crait une instance de la classe effectuant la conversion souhaitée
+	/**1.On crait une instance de la classe effectuant la conversion souhaitée
 	 * 2.On fait appel à la méthode permettant d'effectuer le parse en lui
 	 * donnant en paramètre le fichier à traiter 3.On exécute le code 4.On
 	 * raffraichit le projet 5.Le resultat de la conversion pourra être 
-	 * consulté dans le fichier créé apres execution. 
-	 * ConversionJSONToXML.xml pour une
-	 * conversion de Json à Moodle XML ConversionXMLToJSON.json pour une
-	 * conversion de Moodle XML à Json
+	 * consulté dans le fichier créé apres execution.
+	 * ConversionXMLToJSON.json pour une conversion de Moodle XML à Json
 	 * @param args
 	 * @throws Exception
 	 */
+	public App(){}
 	public static void main(String[] args) throws Exception {
 		XMLtoJSON testXml = new XMLtoJSON();
 		testXml.convertXmlToJson("ExempleXml.xml");
