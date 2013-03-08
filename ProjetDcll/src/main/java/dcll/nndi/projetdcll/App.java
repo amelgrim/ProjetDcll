a75bb8b9cbea7af3a6774eae1e6c819fc000cb9a
grimamel <elataay@hotmail.fr>
2013-03-08 02:48:35 +0100

    ajout de la classe qui permet de faire la conersion de XML to Json ainsi que le fichier a convertir

--- ProjetDcll/src/main/java/dcll/nndi/projetdcll/App.java
+++ ProjetDcll/src/main/java/dcll/nndi/projetdcll/App.java
@@ -3,6 +3,6 @@ package dcll.nndi.projetdcll;
 public class App {
 	public static void main(String[] args) {
 		System.out.println( "Conversion JSON To XML " );   
-		Json_To_Xml convjsonxml=new Json_To_Xml();
+		JsonToXML convjsonxml=new JsonToXML();
 		convjsonxml.convertJsonToXml("fichier.json","sortie.xml"); 
 }
