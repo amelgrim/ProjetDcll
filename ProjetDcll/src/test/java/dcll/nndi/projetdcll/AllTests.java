147eeeca62a85be82480ac690605f739c1ae14fd
etu5 <ndmariane@gmail.com>
2013-03-08 11:18:08 +0100

    mise a jour de la classe AllTests

--- ProjetDcll/src/test/java/dcll/nndi/projetdcll/AllTests.java
+++ ProjetDcll/src/test/java/dcll/nndi/projetdcll/AllTests.java
@@ -3,9 +3,13 @@ package dcll.nndi.projetdcll;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
+/**
+ * automatisation de toutes les classes de test 
+ * @author Ndèye Mariane NDIAYE & Mohamed Amine LOUHIDI
+ *
+ */
 @RunWith(Suite.class)
-@SuiteClasses({ XMLtoJSONTest.class })
+@SuiteClasses({ AppTest.class, XMLtoJSONTest.class })
 public class AllTests {
 
 }
