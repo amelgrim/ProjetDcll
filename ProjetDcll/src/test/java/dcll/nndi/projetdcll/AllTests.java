e019097849c4ae36d396de7f182ee067e578b76d
etu5 <ndmariane@gmail.com>
2013-03-08 11:12:28 +0100

    creation de classe de test pour App et de la classe permettant d'effectuer tous  les tests automatiquement

---
+++ ProjetDcll/src/test/java/dcll/nndi/projetdcll/AllTests.java
@@ -0,0 +1,11 @@
+package dcll.nndi.projetdcll;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ XMLtoJSONTest.class })
+public class AllTests {
+
+}
