4a4ac3e4d82429f33349ff4d1c5023e5273cf04c
Diallo Abdourahamane <abdd.diallo@gmail.com>
2013-03-08 03:47:01 +0100

    Correction des conflits: correction de l'erreur checkstyle qui disait que les utility ne doivent pas avoir de constructeur, comme il n'a pas aime que la classe soit declare private alors on l'a declare enum ce qui convient a checkstyle et qui permet de ne pas pouvoir instancie cette classe

--- ProjetDcll/src/main/java/dcll/nndi/projetdcll/App.java
+++ ProjetDcll/src/main/java/dcll/nndi/projetdcll/App.java
@@ -1,6 +1,6 @@
 package dcll.nndi.projetdcll;
 
-<<<<<<< HEAD
+
 /**
  * Classe pour tester les convertions.
  * @author abddiallo amelGrim
@@ -20,11 +20,4 @@ public enum App {;
         JsonToXML convjsonxml = new JsonToXML();
         convjsonxml.convertJsonToXml("fichier.json", "sortie.xml");
     }
-=======
-public class App {
-	public static void main(String[] args) {
-		System.out.println( "Conversion JSON To XML " );   
-		JsonToXML convjsonxml=new JsonToXML();
-		convjsonxml.convertJsonToXml("fichier.json","sortie.xml"); 
->>>>>>> c8cd36f922930f8b13b8a38bce78cf257786fb8e
 }
