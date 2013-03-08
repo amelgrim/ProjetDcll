c8cd36f922930f8b13b8a38bce78cf257786fb8e
grimamel <elataay@hotmail.fr>
2013-03-08 02:15:57 +0100

    ajout de la classe Json to Xml, ainsi que le fichier Json a convertir

--- ProjetDcll/src/main/java/dcll/nndi/projetdcll/App.java
+++ ProjetDcll/src/main/java/dcll/nndi/projetdcll/App.java
@@ -2,6 +2,7 @@ package dcll.nndi.projetdcll;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+		System.out.println( "Conversion JSON To XML " );   
+		Json_To_Xml convjsonxml=new Json_To_Xml();
+		convjsonxml.convertJsonToXml("fichier.json","sortie.xml"); 
 }
